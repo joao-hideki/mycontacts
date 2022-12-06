0b0b36590c1c957187e3579b1ad6e66f83c2f01b
joao-hideki <joaoohideki12345@gmail.com>
2022-12-06 19:57:50 -0300

    permitindo os tipos de métodos HTTPS e headers que ferem as regras do simple request ao fazer uma request do tipo CORS

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,4 +1,6 @@
 module.exports = (request, response, next) => {
   response.setHeader('Acess-Controll-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Acess-Controll-Allow-Methods', '*');
+  response.setHeader('Acess-Controll-Allow-Headers', '*');
   next();
 };
