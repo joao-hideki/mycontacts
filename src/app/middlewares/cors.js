ea3c6097a0b97418dc46a70e8ea4ef271e84c627
joao-hideki <joaoohideki12345@gmail.com>
2022-12-06 20:10:00 -0300

    cacheando resultado do preflight

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -2,5 +2,6 @@ module.exports = (request, response, next) => {
   response.setHeader('Acess-Controll-Allow-Origin', 'http://localhost:3000');
   response.setHeader('Acess-Controll-Allow-Methods', '*');
   response.setHeader('Acess-Controll-Allow-Headers', '*');
+  response.setHeader('Acess-Controll-Max-Age', '10');
   next();
 };
